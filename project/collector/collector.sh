baa75f6a70dc73a803d1b1258085790478d3715e
mrdizz23 <116642821+mrdizz23@users.noreply.github.com>
2025-12-09 17:28:56 +0500

    Update collector.sh

--- project/collector/collector.sh
+++ project/collector/collector.sh
@@ -7,12 +7,12 @@ connection_string="psql -qtAX postgresql://stat_monitor@localhost:5432/postgres"
 stats_connect="psql -qtAX postgresql://statements_owner@statements:5432/statements"
 
 # формирование инсертов отобранной статистики во временный файл
-stats=$($connection_string -f "/app/get_stats.sql" > /tmp/stat_statements/$pg_port.tmp)
+stats=$($connection_string -f "/app/get_stats.sql" > /tmp/stat_statements.tmp)
 
 # сброс статистики после отбора
 reset=$($connection_string -c "SELECT pg_stat_statements_reset();")
 
 # пуш отобранной статистики в служебную БД
-result=$($stats_connect -f "/tmp/stat_statements/$pg_port.tmp")
+result=$($stats_connect -f "/tmp/stat_statements.tmp")
 
 echo 0
